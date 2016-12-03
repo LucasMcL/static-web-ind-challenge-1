1685c835e78bac0a4479ba9507c7158dd3e0fd63
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-03 16:12:21 -0600

    completed challenge

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-var sliderEl = document.getElementById('slider');
-var sliderOutputEl = document.getElementById('slider_output');
+// Challenge #1
 
-var changeOutput = function() {
-  sliderOutputEl.value = sliderEl.value;
+// Create a form that has a range input, and a number input,
+//both with the same values for min, max, and default.
+//When the user changes the value on the range input,
+//the value of the number input should match.
+//The opposite should be true.
+//The values of the two inputs should always be synchronized.
+
+var sliderEl = document.getElementById('slider');
+var numberEl = document.getElementById('number');
+
+
+var changeSlider = function() {
+  sliderEl.value = numberEl.value;
 }
 
+var changeNumber = function() {
+  numberEl.value = sliderEl.value;
+}
 
-sliderEl.addEventListener("input", changeOutput);
+sliderEl.addEventListener("input", changeNumber);
+numberEl.addEventListener("input", changeSlider);
